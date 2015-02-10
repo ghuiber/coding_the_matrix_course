8e2d312df99aea4ff43d00a2aa08d7abea6894a9
Tianpu Han <hantempo@gmail.com>
2015-02-10 21:10:23 +0100

    Finish the vector problems 1-3

--- The_Vector_problems.py
+++ The_Vector_problems.py
@@ -12,26 +12,26 @@ from GF2 import one
 #Please express each answer as a list of numbers
 p1_v = [-1, 3]
 p1_u = [0, 4]
-p1_v_plus_u = [...]
-p1_v_minus_u = [...]
-p1_three_v_minus_two_u = [...]
+p1_v_plus_u = [-1, 7]
+p1_v_minus_u = [-1, -1]
+p1_three_v_minus_two_u = [-3, 1]
 
 
 
 ## 2: (Problem 2) Vector Addition Practice 2
 p2_u = [-1,  1, 1]
 p2_v = [ 2, -1, 5]
-p2_v_plus_u = [...]
-p2_v_minus_u = [...]
-p2_two_v_minus_u = [...]
-p2_v_plus_two_u = [...]
+p2_v_plus_u = [1, 0, 6]
+p2_v_minus_u = [3, -2, 4]
+p2_two_v_minus_u = [5, -3, 9]
+p2_v_plus_two_u = [0, 1, 7]
 
 
 
 ## 3: (Problem 3) Vector Addition Practice 3
 # Write your answer using GF2's one instead of the number 1
-p3_vector_sum_1 = [...]
-p3_vector_sum_2 = [...]
+p3_vector_sum_1 = [one, 0, 0]
+p3_vector_sum_2 = [0, one, one]
 
 
 
